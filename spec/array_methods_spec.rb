cb4098cafd2bdadf51d6781c71ca697761488ace
John Grubbs <johnmlowery@gmail.com>
2018-08-20 15:50:45 +0000

    automatically backed up by learn

--- spec/array_methods_spec.rb
+++ spec/array_methods_spec.rb
@@ -5,7 +5,6 @@ describe "#using_include" do
     array = ["wow", "I", "am", "really", "learning", "arrays!"]
     element1 = "wow"
     element2 = "Tardis"
-    
     expect(using_include(array, element2)).to eq(false)
   end
 end
